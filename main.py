2826149f236efe3a347ee514e381dc5330ee2209
Oeun Nuphea <nupheaoeun@gmail.com>
2025-10-22 17:44:28 +0700

    update

--- main.py
+++ main.py
@@ -102,30 +102,30 @@ def github_webhook():
     # ==============================
     # PULL REQUEST MERGED EVENT
     # ==============================
-    if event_type == "pull_request":
-        action = data.get("action")
-        pr = data.get("pull_request", {})
-        sender = data.get("sender", {}).get("login", "")
-        pr_title = pr.get("title", "")
-        pr_number = pr.get("number", "")
-        merged = pr.get("merged", False)
-        head_branch = pr.get("head", {}).get("ref", "")
-        base_branch = pr.get("base", {}).get("ref", "")
+    # if event_type == "pull_request":
+    #     action = data.get("action")
+    #     pr = data.get("pull_request", {})
+    #     sender = data.get("sender", {}).get("login", "")
+    #     pr_title = pr.get("title", "")
+    #     pr_number = pr.get("number", "")
+    #     merged = pr.get("merged", False)
+    #     head_branch = pr.get("head", {}).get("ref", "")
+    #     base_branch = pr.get("base", {}).get("ref", "")
 
-        # Only alert if merged
-        if action == "closed" and merged:
-            message = (
-                f"🎉 Pull Request *#{pr_number} {head_branch} → {base_branch}* merged\n"
-                f"🎃 By: {sender}\n"
-                f"📦 Repo: {repo}\n"
-                f"🕒 {kh_time}"
-            )
-            send_message(message)
-            return {"status": "pull_request merged received"}
-        else:
-            # Ignore other pull request actions
-            return {"status": f"pull_request {action} ignored"}
-    return {"status": f"pull_request {action} ignored"}
+    #     # Only alert if merged
+    #     if action == "closed" and merged:
+    #         message = (
+    #             f"🎉 Pull Request *#{pr_number} {head_branch} → {base_branch}* merged\n"
+    #             f"🎃 By: {sender}\n"
+    #             f"📦 Repo: {repo}\n"
+    #             f"🕒 {kh_time}"
+    #         )
+    #         send_message(message)
+    #         return {"status": "pull_request merged received"}
+    #     else:
+    #         # Ignore other pull request actions
+    #         return {"status": f"pull_request {action} ignored"}
+    # return {"status": f"pull_request {action} ignored"}
 
     # ==============================
     # DEFAULT UNKNOWN EVENT
